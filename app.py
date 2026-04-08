f0e1c8a165f71f50498730821b926b624e7b831d
sakshipatel170912-lgtm <sakshipatel170912@gmail.com>
2026-04-08 08:47:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
-app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSSWORD")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 
 # ================= CORS FIX =================
 CORS(
